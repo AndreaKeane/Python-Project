74238e7dc6b4444364cd6eb3efa21d73f995b899
Andrea Keane <a.living.keane@gmail.com>
2024-04-11 13:52:48 -0600

    Tests reorganized

--- project/database.py
+++ project/database.py
@@ -1,28 +1,10 @@
 from pymongo import MongoClient
-import os
 
 
-def mongo_client(): 
+def mongo(): 
     """Establish a MongoDB Connection."""
 
-    # Using environment variables so our test fixtures can connect to a test instance
-    host = os.getenv("MONGO_HOST", "localhost")
-    port = os.getenv("MONGO_PORT", 27017)
-
     # Return the Mongo connection
-    client = MongoClient(host=host, port=port)
+    client = MongoClient(host="localhost", port=27017)
     print(f"Connected To Mongo Client @ {client.HOST}:{client.PORT}")
     return client
-
-
-def mongo_db(db_name=None): 
-    """Connect to a MongoDB Database using Env Variables"""
-    # Keeping the env variable lookups in one place.
-
-    # First, check for a DB name stored in os.getenv("MONGO_DB_NAME") - used by pytest
-    # Then, if the env variable is empty, use the DB name that's passed in
-    list = [os.getenv("MONGO_DB_NAME"), db_name]
-    db_name = next(name for name in list if name)
-
-    # Connect to the database name identified
-    return mongo_client()[db_name]
--- project/main.py
+++ project/main.py
@@ -1,14 +1,15 @@
 
-import database
+from project import database
 import logging
 
 
 def do_stuff(desc="default placeholder text") -> dict: 
     """Placeholder for interacting with MongoDB"""
 
-    db = database.mongo_db()
+    client = database.mongo()
+    db = client["tbd"]
     collection = db.name_this_collection
-    print(f"Connected to Mongo {db.name}:{collection.name}")
+    logging.info(f"Connected to Mongo {db.name}:{collection.name}")
 
     # Insert test data 
     data1 = {
@@ -16,11 +17,11 @@ def do_stuff(desc="default placeholder text") -> dict:
         "description": desc
     }
     data1_id = collection.insert_one(data1).inserted_id
-    print(f"Inserted ID: {data1_id}")
+    logging.info(f"Inserted ID: {data1_id}")
 
     # Read data from db 
     data1_response = collection.find_one(data1_id)
-    print(f"found{data1_response}")
+    logging.info(f"found{data1_response}")
 
     return data1_response
 
